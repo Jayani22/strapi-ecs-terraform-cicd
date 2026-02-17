b36ec3a6b60e78aff6b7f3a5b199b15130d81895
Jayani22 <mutcherlajayani@gmail.com>
2026-02-17 15:15:33 +0530

    Add GitHub Actions CI/CD pipeline

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -24,3 +24,27 @@ resource "aws_subnet" "public_2" {
   availability_zone       = "${var.aws_region}b"
   map_public_ip_on_launch = true
 }
+
+# Route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+}
+
+# Internet route
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+# Associate subnet 1
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate subnet 2
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
