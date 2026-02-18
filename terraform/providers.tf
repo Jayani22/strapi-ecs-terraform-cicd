7cd1a6025bb22a19f116eda6eeb9df51967e719f
Jayani22 <mutcherlajayani@gmail.com>
2026-02-18 15:13:28 +0530

    trigger deployment

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "385046010663-bucket"
+    bucket         = "pearlthoughts-strapi-tfstate"
     key            = "strapi/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
