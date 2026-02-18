b4f28f9d0fb0052b1f271e2e0da96749e57dcd88
Jayani22 <mutcherlajayani@gmail.com>
2026-02-18 16:03:22 +0530

    trigger workflow

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  # backend "s3" {
-  #   bucket         = "pearlthoughts-strapi-tfstate"
-  #   key            = "strapi/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   encrypt        = true
-  # }
+  backend "s3" {
+    bucket         = "pearlthoughts-strapi-tfstate"
+    key            = "strapi/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
