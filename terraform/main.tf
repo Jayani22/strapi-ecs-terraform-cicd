400e2f3f67494f32d22698f9ba0d0a02b485d4f8
Jayani22 <mutcherlajayani@gmail.com>
2026-02-18 16:19:13 +0530

    trigger workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,5 @@
-module "vpc" {
-    source       = "./modules/vpc"
-    project_name = var.project_name
-    aws_region   = var.aws_region
-}
-
 module "ecr" {
     source       = "./modules/ecr"
-    project_name = var.project_name
-}
-
-module "iam" {
-    source       = "./modules/iam"
     project_name = var.project_name
 }
 
